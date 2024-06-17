83aed655059819e0222c781b25cdb67d30a648e0
Ranielle Lloyd Cruz <raniellecruz16@gmail.com>
2024-06-17 16:02:56 +0800

    init

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!isset($_SERVER['HTTP_REFERER'])) {
+    //redirect them to your desired location
+    header('location: http://localhost/freshcery/index.php');
+    exit;
+}
+$servername = "localhost";
+$username = "root";
+$password = "ranielle25";
+
+try {
+    //connect db
+    $conn = new PDO("mysql:host" . $servername . ";dbname=freshcery", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    // Handle connection errors
+    echo "Connection failed: " . $e->getMessage();
+}
