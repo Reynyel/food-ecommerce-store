d1dbefaa8d4a9fca6802af246a4531dc3d2c1e2d
Ranielle Lloyd Cruz <raniellecruz16@gmail.com>
2024-06-18 17:04:10 +0800

    add user auth

--- config/config.php
+++ config/config.php
@@ -1,19 +1,19 @@
 <?php
 
-if (!isset($_SERVER['HTTP_REFERER'])) {
-    //redirect them to your desired location
-    header('location: http://localhost/freshcery/index.php');
-    exit;
-}
+// if (!isset($_SERVER['HTTP_REFERER'])) {
+//     //redirect them to your desired location
+//     header('location: http://localhost/freshcery/index.php');
+//     exit;
+// }
 $servername = "localhost";
 $username = "root";
 $password = "ranielle25";
 
 try {
     //connect db
-    $conn = new PDO("mysql:host" . $servername . ";dbname=freshcery", $username, $password);
+    $conn = new PDO("mysql:host=" . $servername . ";port=3307;dbname=freshcery", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    // echo "Connected successfully";
 } catch (PDOException $e) {
     // Handle connection errors
     echo "Connection failed: " . $e->getMessage();
